3f2868ec1f3702ef50ce88f70a1b49ad225592f8
Salah <2236291956@qq.com>
2022-10-12 16:04:24 +0800

    修改导航栏显示 修改样式单位代码 进行适配

--- src/components/NavigatorBar/index.tsx
+++ src/components/NavigatorBar/index.tsx
@@ -1,11 +1,4 @@
-/*
- * @Author: 'Salah' '2236291956@qq.com'
- * @Date: 2022-09-26 17:08:53
- * @LastEditors: 'Salah' '2236291956@qq.com'
- * @LastEditTime: 2022-10-11 16:27:30
- * @FilePath: \FIFA Wolrd Cup\src\components\NavigatorBar\index.tsx
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
+
 /* eslint-disable react-hooks/rules-of-hooks */
 import React, { useEffect, useState } from 'react'
 
@@ -121,7 +114,7 @@ export default function Index(props) {
               >SWAP</span>
             </div>
           </div>
-          <div >
+          <div className='menuArea'>
             <Menu />
           </div>
         </div>
