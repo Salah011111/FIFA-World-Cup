b0030792917f1a4d1284eae57359725af46f2346
Salah <2236291956@qq.com>
2022-10-13 11:53:01 +0800

    完成tournament页面 导航栏下方页面第一部分和 倒计时下方图片显示区域UI的开发

--- src/pages/Touranment/index.tsx
+++ src/pages/Touranment/index.tsx
@@ -5,7 +5,7 @@
  * @Author: 'Salah' '2236291956@qq.com'
  * @Date: 2022-09-26 18:30:38
  * @LastEditors: 'Salah' '2236291956@qq.com'
- * @LastEditTime: 2022-09-29 11:55:44
+ * @LastEditTime: 2022-10-13 10:42:41
  * @FilePath: \FIFA Wolrd Cup\src\pages\Home\index.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -130,6 +130,11 @@ export default function Index() {
 
             <div className="coverImage">
                 <div className="worldCupImage" />
+
+                <div className="PhonetextButtonBlock">
+                    <span>fifa word cup qatar 2022™</span>
+                    <span>the official home for the word's most prestigious event</span>
+                </div>
             </div>
         </div>
 
