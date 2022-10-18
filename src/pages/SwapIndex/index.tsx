49effe51468cad17019e4dd0fbcc8f64942a200f
Salah <2236291956@qq.com>
2022-10-18 14:23:12 +0800

    完善移除流动性移动端适配

--- src/pages/SwapIndex/index.tsx
+++ src/pages/SwapIndex/index.tsx
@@ -6,7 +6,7 @@
  * @Author: 'Salah' '2236291956@qq.com'
  * @Date: 2022-09-27 13:48:36
  * @LastEditors: 'Salah' '2236291956@qq.com'
- * @LastEditTime: 2022-10-17 15:34:57
+ * @LastEditTime: 2022-10-18 13:43:11
  * @FilePath: \FIFA Wolrd Cup\src\pages\Swap\index.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -781,10 +781,11 @@ export default function index(props) {
               <span className="SwapFooterRightText">Copyight © 1994-2022 FIFA. All rights reserved</span>
             </div>
           </div>
+
+          <PhoneFooter footerIndex='3'/>
         </div>
       </div>
 
-      <PhoneFooter footerIndex='3'/>
     </div>
   )
 }
