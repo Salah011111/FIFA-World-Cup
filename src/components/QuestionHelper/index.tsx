bc3329f51e97bcd050861598e739076ea7f3257f
Salah <2236291956@qq.com>
2022-10-19 09:04:45 +0800

    暂时关闭问题图标的悬浮窗提示 以便于测试修改UI

--- src/components/QuestionHelper/index.tsx
+++ src/components/QuestionHelper/index.tsx
@@ -1,3 +1,11 @@
+/*
+ * @Author: 'Salah' '2236291956@qq.com'
+ * @Date: 2022-09-26 15:40:38
+ * @LastEditors: 'Salah' '2236291956@qq.com'
+ * @LastEditTime: 2022-10-18 17:59:47
+ * @FilePath: \cypress\src\components\QuestionHelper\index.tsx
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import React, { useCallback, useState } from 'react'
 import { HelpCircle as Question } from 'react-feather'
 import styled from 'styled-components'
@@ -32,7 +40,9 @@ export default function QuestionHelper({ text }: { text: string }) {
   return (
     <span style={{ marginLeft: 4 }}>
       <Tooltip text={text} show={show}>
-        <QuestionWrapper onClick={open} onMouseEnter={open} onMouseLeave={close}>
+        {/* 删除关闭事件 */}
+      <QuestionWrapper onClick={open} onMouseEnter={open}>
+        {/* <QuestionWrapper onClick={open} onMouseEnter={open} onMouseLeave={close}> */}
         {/* <QuestionWrapper onClick={open} onMouseEnter={open}> */}
           <Question size={16} />
         </QuestionWrapper>
